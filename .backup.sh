dbcaf08abae81bd2d6c438d0c3d63f1db8752e0c
Bryan White <bryanchriswhite@gmail.com>
2016-01-28 03:14:51 -0600

    refactor/bugfixing: don't assume `$HOME` dirname for inputs

--- .backup.sh
+++ .backup.sh
@@ -11,17 +11,19 @@ PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $PWD/.config
 
 
-### Create temp directory and make it cwd (needed for clean tarballs)
+### Create temp directory
 
 TEMP_DIR=`mktemp -d /tmp/.backup.XXXX`
-cd $TEMP_DIR
 
 
 ### Tar, xz compress, and asymmetricly encrypt files
 
 for ASYM_INPUT in $ASYM_INPUTS
 do
-	tar -cvJ $HOME/$ASYM_INPUT 2>/dev/null | gpg -er $RECIPIENTS -o $TEMP_DIR/$ASYM_INPUT.txz.gpg
+  BASENAME=`basename $ASYM_INPUT`
+  DIRNAME=`dirname $ASYM_INPUT`
+  cd $DIRNAME
+	tar -cJ $BASENAME 2>/dev/null | gpg -er $RECIPIENTS -o $TEMP_DIR/$BASENAME.txz.gpg
 done
 
 
@@ -29,10 +31,19 @@ done
 
 for SYM_INPUT in $SYM_INPUTS
 do
+  BASENAME=`basename $SYM_INPUT`
+  DIRNAME=`dirname $SYM_INPUT`
+  cd $DIRNAME
   read -sp "Enter passphrase for $SYM_INPUT: " PASSPHRASE
   echo
-  tar -cvJ $HOME/$SYM_INPUT 2>/dev/null | gpg --passphrase $PASSPHRASE -c -o $TEMP_DIR/$SYM_INPUT.txz.gpg
+  cd $DIRNAME
+  tar -cJ $BASENAME 2>/dev/null | gpg --passphrase $PASSPHRASE -c -o $TEMP_DIR/$BASENAME.txz.gpg
 done
+
+
+### cd to temp directory for clean tarball
+
+cd $TEMP_DIR
 
 
 ### Create final tarball of encrypted files
