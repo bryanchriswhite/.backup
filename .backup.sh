744ed48bd72519f2ca07f0cf9092bb2833b25206
Bryan White <bryanchriswhite@gmail.com>
2016-01-28 02:00:27 -0600

    more refactoring & commenting

--- .backup.sh
+++ .backup.sh
@@ -7,19 +7,21 @@ PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $PWD/.config
 
 
+### Create temp directory and make it cwd (needed for clean tarballs)
+
 TEMP_DIR=`mktemp -d /tmp/.backup.XXXX`
 cd $TEMP_DIR
 
 
-### Asymmetricly encrypted files
+### Tar, xz compress, and asymmetricly encrypt files
 
 for ASYM_INPUT in $ASYM_INPUTS
 do
-	tar -cvJ $HOME/$ASYM_INPUT 2>/dev/null | gpg -er bryanchriswhite@gmail.com -o $TEMP_DIR/$ASYM_INPUT.txz.gpg
+	tar -cvJ $HOME/$ASYM_INPUT 2>/dev/null | gpg -er $RECIPIENTS -o $TEMP_DIR/$ASYM_INPUT.txz.gpg
 done
 
 
-### Symmetricly encrypted files
+### Tar, xz compress, and symmetricly encrypt files
 
 for SYM_INPUT in $SYM_INPUTS
 do
@@ -28,7 +30,13 @@ do
   tar -cvJ $HOME/$SYM_INPUT 2>/dev/null | gpg --passphrase $PASSPHRASE -c -o $TEMP_DIR/$SYM_INPUT.txz.gpg
 done
 
+
+### Create final tarball of encrypted files
+
 tar -cvf $BACKUP_DIR/.keys_backup.tar .*.gpg
 chmod 600 $BACKUP_DIR/.keys_backup.tar
+
+
+### Cleanup temp directory
 
 rm -rf $TEMP_DIR
